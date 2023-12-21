2387a2a1896eefeaf7a4f38e3e1a1d283a8cb4db
AndreaPaolo <2000.andrea.paolo@gmail.com>
2023-12-21 21:34:23 +0100

    Setup java springboot

---
+++ backend/src/main/java/com/AP/Aesthetich_Performance/AesthetichPerformanceApplication.java
@@ -0,0 +1,14 @@
+package com.AP.Aesthetich_Performance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AesthetichPerformanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AesthetichPerformanceApplication.class, args);
+		System.out.println("Ciao");
+	}
+
+}
---
+++ backend/src/test/java/com/AP/Aesthetich_Performance/AesthetichPerformanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AP.Aesthetich_Performance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AesthetichPerformanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
